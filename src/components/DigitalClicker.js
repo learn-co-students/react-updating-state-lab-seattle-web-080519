06c379211b5ee73325929d84e4a262ff61bc5046
github username <github email address>
2019-10-10 09:41:02 -0700

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,25 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+export default class DigitalClicker extends React.Component{
+
+    constructor(){
+        super();
+        this.state = {
+            timesClicked: 0
+        };
+    }
+
+    // plusss(){
+    //     console.log(1+2)
+    // }
+
+    handleClick (event) {
+        // console.log(this.state)
+        this.setState({timesClicked: this.state.timesClicked+1});
+    }
+
+    render () {
+        return (<div><button onClick={(event) => this.handleClick(event)}>{this.state.timesClicked}</button><br/></div>);
+    }
+}
